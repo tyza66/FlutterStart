82de53ef3d410b6e1f4c53091a7b659bff6662c8
洮羱芝闇 <shun_@outlook.com>
2022-06-06 14:24:38 +0800

    update

--- flutterstart/lib/main.dart
+++ flutterstart/lib/main.dart
@@ -2,20 +2,33 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Welcom to Flutter",
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Welcom to Flutter',textAlign: TextAlign.center,),
+          title: const Text(
+            'Welcom to Flutter',
+            textAlign: TextAlign.center,
+          ),
         ),
-        body: Center(
-          child: Text("Hello World111111111111111111111111111111111111111111111111111111111111",textAlign: TextAlign.left,),
+        body: const Center(
+          child: Text(
+            "Hello111 World111111111111111111111111111111111111111111111111111111111111",
+            textAlign: TextAlign.left,
+            maxLines: 1,
+            overflow: TextOverflow.ellipsis,
+            style: TextStyle(
+              fontSize: 25.0,
+              color: Color.fromARGB(255, 255, 125, 125),
+              decoration: TextDecoration.underline,
+              decorationStyle: TextDecorationStyle.solid
+            ),
+          ),
         ),
       ),
     );
   }
-  
 }
