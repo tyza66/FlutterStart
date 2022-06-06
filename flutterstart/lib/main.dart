b3745a767c5400648397236ba0f630288cef2d8f
洮羱芝闇 <shun_@outlook.com>
2022-06-06 15:15:31 +0800

    update

--- flutterstart/lib/main.dart
+++ flutterstart/lib/main.dart
@@ -8,14 +8,17 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: "Welcom to Flutter",
       home: Scaffold(
+        /*
         appBar: AppBar(
           title: const Text(
             'Welcom to Flutter',
             textAlign: TextAlign.center,
           ),
         ),
+        */
         body: Center(
             child: Container(
+          /*
           alignment: Alignment.topLeft,
           width: 500,
           height: 400,
@@ -28,7 +31,31 @@ class MyApp extends StatelessWidget {
                 colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]),
             border: Border.all(width: 2.0, color: Colors.red),
           ),
-          child: const Text("tyza66", style: TextStyle(fontSize: 40)),
+          */
+          width: 300,
+          height: 200,
+          color: Colors.lightBlue,
+          /*
+          alignment: Alignment.topLeft,
+          width: 500,
+          height: 400,
+          //color: Colors.lightBlue,
+          padding: const EdgeInsets.fromLTRB(10.0, 30.0, 0, 0),
+          margin: const EdgeInsets.all(10.0),
+          decoration: BoxDecoration(
+            gradient: const LinearGradient(
+                // ignore: prefer_const_literals_to_create_immutables
+                colors: [Colors.lightBlue, Colors.greenAccent, Colors.purple]),
+            border: Border.all(width: 2.0, color: Colors.red),
+          ),
+          */
+          child: Image.network(
+            'https://i0.hdslb.com/bfs/face/9517a7cb97a237d3548de571ba61e1cc62731c0c.jpg@240w_240h_1c_1s.webp',
+            fit: BoxFit.contain,
+            color: Colors.lightBlue,
+            colorBlendMode: BlendMode.darken,
+            repeat: ImageRepeat.noRepeat,
+          ),
         )
             /*Text(
             "Hello111 World111111111111111111111111111111111111111111111111111111111111",
@@ -38,7 +65,7 @@ class MyApp extends StatelessWidget {
             style: TextStyle(
               fontSize: 25.0,
               color: Color.fromARGB(255, 255, 125, 125),
-              decoration: TextDecoration.underline,
+              decoration: TextDecoration.underline, 
               decorationStyle: TextDecorationStyle.solid
             ),
           ),*/
